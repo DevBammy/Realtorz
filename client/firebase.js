6f53f4ced2b53637ed2b9fb5ab9a091e3a083791
Bamidele <devbammy@gmail.com>
2024-03-24 20:06:30 +0100

    firebase env

--- client/firebase.js
+++ client/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBJCz5okHaPbPPasVUB5LQ3XmGOo0RHN_c',
+  apiKey: import.meta.env.VITE_FIREBASE_APP,
   authDomain: 'realtorz-web-app.firebaseapp.com',
   projectId: 'realtorz-web-app',
   storageBucket: 'realtorz-web-app.appspot.com',
