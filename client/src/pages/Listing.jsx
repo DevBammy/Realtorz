2adff42a2d2fb7643ae0ff5d0535bf0293384ede
Bamidele <devbammy@gmail.com>
2024-03-26 00:30:19 +0100

    added listing images functionality

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,4 +1,72 @@
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
+import toast from 'react-hot-toast';
+import { useState } from 'react';
+import { app } from '../../firebase';
+
 const Listing = () => {
+  const [files, setFiles] = useState();
+  const [formData, setFormData] = useState({
+    imageUrls: [],
+  });
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleImageUplaod = (e) => {
+    setIsLoading(true);
+    if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+      const promises = [];
+
+      for (let i = 0; i < files.length; i++) {
+        promises.push(storeImage(files[1]));
+      }
+      Promise.all(promises)
+        .then((urls) => {
+          setFormData({
+            ...formData,
+            imageUrls: formData.imageUrls.concat(urls),
+          });
+          setIsLoading(false);
+          toast.success('Images uploaded successfully');
+        })
+        .catch((error) => {
+          toast.error('error uploading images (max 2MB per image)');
+          setIsLoading(false);
+        });
+    } else {
+      toast.error('you can only upload 6 images ');
+    }
+  };
+
+  const storeImage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(progress);
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
+    });
+  };
+
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
@@ -123,9 +191,14 @@ const Listing = () => {
               id="images"
               accept="image/*"
               multiple
+              onChange={(e) => setFiles(e.target.files)}
             />
-            <button className="p-3 text-white border-0 outline-0  my-4 bg-green-700 rounded uppercase hover:opacity-90 disabled:opacity-70">
-              Upload
+            <button
+              type="button"
+              onClick={handleImageUplaod}
+              className="p-3 text-white border-0 outline-0  my-4 bg-green-700 rounded uppercase hover:opacity-90 disabled:opacity-70"
+            >
+              {isLoading ? 'Uploading...' : 'Upload'}
             </button>
           </div>
           <button
